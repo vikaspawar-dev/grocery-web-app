be39d2f50680b0c50a93f47f3eeb9d4e6c86a531
Vikas Pawar <pvikasrmc@gmail.com>
2026-05-27 10:03:09 +0530

    Refactor routing to use HashRouter and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./components/Home/Home";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import Fruits from "./components/Fruits/Fruits";
 import Dairy from "./components/Dairy/Dairy";
 import SeaFood from "./components/SeaFood/SeaFood";
@@ -11,40 +9,19 @@ import AllProducts from "./components/AllProducts/AllProducts";
 import Layout from "./components/Layout/Layout";
 
 function App() {
-
-  const router = createBrowserRouter([
-
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        {
-          path:'/',
-          element: <Home />
-        },
-        {
-          path: '/fruits',
-          element: <Fruits />
-        },
-        {
-          path: '/dairy',
-          element: <Dairy />
-        },
-        {
-          path: '/seafood',
-          element: <SeaFood />
-        },
-        {
-          path: '/allproducts',
-          element: <AllProducts />
-        },
-
-      ]
-    },
-
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="fruits" element={<Fruits />} />
+          <Route path="dairy" element={<Dairy />} />
+          <Route path="seafood" element={<SeaFood />} />
+          <Route path="allproducts" element={<AllProducts />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
